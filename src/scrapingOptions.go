b0e6e870cf3bd809a5e697fa46a39cb2648a9e27
Stephen Lineker-Miller <stephen.lineker-miller@qgate.co.uk>
2019-09-02 13:12:08 +0100

    Command line parsing
    basic commandline parsing of the required fields apart from the subreddits. This is is because go does not parse []string well that I know of yet.

---
+++ src/scrapingOptions.go
@@ -0,0 +1,22 @@
+package main
+
+type ScrapingOptions struct {
+	//  The directory in which we will be downloading all the images into, based on the folder name
+	//  of the given sub-reddit.
+	outputDirectory string
+
+	// The total number of images to download max per sub-reddit before we continue to the next one.
+	imageLimit int
+
+	// If set to true, the tool will scrape the front page of reddit for the current most active
+	// sub-reddits and then scrape all the links directly from them sub-reddits.
+	frontPage bool
+
+	// You can change this to adjust on what kind of images you get from Reddits filtering options
+	// (hot, new, rising, controversial, top), hot is the default by reddit while also the default
+	// in the tool.
+	pageType string
+
+	// What subreddits are going to be scrapped for downloading of sad images.
+	subreddits []string
+}
