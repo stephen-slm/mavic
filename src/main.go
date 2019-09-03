55824937fb27ab37be8a9272b610244ebdf2c0ce
Stephen Lineker-Miller <stephen.lineker-miller@qgate.co.uk>
2019-09-03 13:07:17 +0100

    scrapingOptions direct parse
    parse is done directly with a reciever method on the scraping options method to improve the process.

--- src/main.go
+++ src/main.go
@@ -3,23 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
 )
 
-func parseCommandLineArguments(arguments []string) ScrapingOptions {
-	CommandLine := flag.CommandLine
+func generateScrapingOptions(arguments []string) ScrapingOptions {
 	scrapingOptions := ScrapingOptions{}
-
-	CommandLine.StringVar(&scrapingOptions.outputDirectory, "OutputDirectory", "./", "The output directory to store the images.")
-	CommandLine.IntVar(&scrapingOptions.imageLimit, "ImageLimit", 50, "The output directory to store the images.")
-	CommandLine.BoolVar(&scrapingOptions.frontPage, "FrontPage", false, "The output directory to store the images.")
-
-	CommandLine.Parse(arguments)
-
+	scrapingOptions.parse(arguments)
 	return scrapingOptions
 }
 
 func main() {
-	scrapingOptions := parseCommandLineArguments(os.Args)
+	scrapingOptions := generateScrapingOptions(flag.Args())
 	fmt.Println(scrapingOptions)
 }
