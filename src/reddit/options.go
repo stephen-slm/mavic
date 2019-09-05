d4fca50bd90d85f7373f57daf8e7acaf55a98a16
Stephen Lineker-Miller <slinekermiller@gmail.com>
2019-09-05 21:33:10 +0100

    urfave/cli
    Added in urfave/cli for better and improved cli support, required is
    to just wire up the action since the action does not seem to be
    acting as expected.

--- src/reddit/options.go
+++ src/reddit/options.go
@@ -1,38 +1,22 @@
 package reddit
 
-import (
-	"flag"
-)
-
 type Options struct {
 	//  The directory in which we will be downloading all the images into, based on the folder name
 	//  of the given sub-reddit.
-	outputDirectory string
+	OutputDirectory string
 
 	// The total number of images to download max per sub-reddit before we continue to the next one.
-	imageLimit int
+	ImageLimit int
 
 	// If set to true, the tool will scrape the front page of reddit for the current most active
 	// sub-reddits and then scrape all the links directly from them sub-reddits.
-	frontPage bool
+	FrontPage bool
 
 	// You can change this to adjust on what kind of images you get from Reddits filtering options
 	// (hot, new, rising, controversial, top), hot is the default by reddit while also the default
 	// in the tool.
-	pageType string
+	PageType string
 
 	// What subreddits are going to be scrapped for downloading of sad images.
-	subreddits []string
-}
-
-// parse takes a list of command line arguments and binds them to the scrapingOptions, using default
-// values if the arguments are not given.
-func (s *Options) Parse(arguments []string) {
-	commandLine := flag.CommandLine
-
-	commandLine.StringVar(&s.outputDirectory, "OutputDirectory", "./", "The output directory to store the images.")
-	commandLine.IntVar(&s.imageLimit, "ImageLimit", 50, "The output directory to store the images.")
-	commandLine.BoolVar(&s.frontPage, "FrontPage", false, "The output directory to store the images.")
-
-	_ = commandLine.Parse(arguments)
+	Subreddits []string
 }
